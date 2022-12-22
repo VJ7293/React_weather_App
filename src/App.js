f996c19c3a4430e8b749fe17d85c4cba30e8dda2
vijay kumar <vjkumar7293@gmail.com>
2022-12-22 14:42:05 +0530

    added a tag to button

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
 import React, { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [query, setQuery] = useState({ q: "berlin" });
+  const [query, setQuery] = useState({ q: "bengaluru" });
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
 
@@ -35,28 +36,32 @@ function App() {
     if (!weather) return "from-cyan-700 to-blue-700";
     const threshold = units === "metric" ? 20 : 60;
     if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
-
+    if (weather.temp <= threshold) {
+      return "from-cyan-300 to-white";
+    }
     return "from-yellow-700 to-orange-700";
   };
 
   return (
-    <div
-      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
-    >
-      <TopButtons setQuery={setQuery} />
-      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+    <div className={`flex bg-${formatBackground()} `}>
+      <div
+        className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+      >
+        <TopButtons setQuery={setQuery} />
+        <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
-      {weather && (
-        <div>
-          <TimeAndLocation weather={weather} />
-          <TemperatureAndDetails weather={weather} />
+        {weather && (
+          <div>
+            <TimeAndLocation weather={weather} />
+            <TemperatureAndDetails weather={weather} />
 
-          <Forecast title="hourly forecast" items={weather.hourly} />
-          <Forecast title="daily forecast" items={weather.daily} />
-        </div>
-      )}
+            <Forecast title="hourly forecast" items={weather.hourly} />
+            <Forecast title="daily forecast" items={weather.daily} />
+          </div>
+        )}
 
-      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+        <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+      </div>
     </div>
   );
 }
